c07876aa5e3b80aacb73788830f50a744642bda9
ef3d0c3e <ef3d0c3e@pundalik.org>
2025-02-16 12:04:03 +0100

    Processing

--- src/compiler/process.rs
+++ src/compiler/process.rs
@@ -1,17 +1,29 @@
 use std::{path::PathBuf, sync::Arc};
 
-use crate::{cache::cache::Cache, parser::parser::Parser};
+use crate::{cache::cache::Cache, parser::{parser::Parser, source::SourceFile, translation::TranslationUnit}};
 
-use super::compiler::{Compiler, Target};
+use super::{compiled::CompiledUnit, compiler::{Compiler, Target}};
 
 /// Processqueue for inputs
 pub struct ProcessQueue
 {
 	inputs: Vec<PathBuf>,
+	outputs: Vec<CompiledUnit>,
 	cache: Arc<Cache>,
 
 	parser: Parser,
 	compiler: Compiler,
+}
+
+pub enum ProcessError
+{
+	GeneralError(String),
+	InputError(String, String),
+}
+
+pub enum ProcessOutputOptions {
+	Directory(String),
+	File(String),
 }
 
 impl ProcessQueue {
@@ -24,9 +36,53 @@ impl ProcessQueue {
 
 		Self {
 			inputs,
+			outputs: vec![],
 			cache,
 			parser,
 			compiler
 		}
+	}
+
+	pub fn process(&mut self, options: ProcessOutputOptions) -> Result<Vec<CompiledUnit>, ProcessError>
+	{
+		match options
+		{
+			ProcessOutputOptions::Directory(dir) => {
+
+			},
+			ProcessOutputOptions::File(file) => {
+				if self.inputs.len() > 1
+				{
+					Err(ProcessError::GeneralError("Single file specified with multiple inputs. Please specify a directory instead".into()))?
+				}
+			},
+		};
+
+		let mut compiled = vec![];
+		for input in &self.inputs {
+			let input_string = input
+				.to_str()
+				.map(|s| s.to_string())
+				.ok_or(
+					ProcessError::GeneralError(format!("Failed to convert {input:#?} to string"))
+					)?;
+
+			// Get mtime
+			let meta = std::fs::metadata(input)
+				.map_err(|err| ProcessError::InputError(input_string, format!("Failed to get metadata for `{input_string}`: {err}")))?;
+
+			let modified = meta
+				.modified()
+				.map_err(|err| ProcessError::InputError(input_string, format!("Unable to query modification time for `{input_string}`: {err}")))?;
+
+			// Create unit
+			let source = Arc::new(SourceFile::new(input_string, None)?);
+			let unit = TranslationUnit::new(&self.parser, source, false, true);
+
+			// TODO: Check if necessary to compile
+			unit = unit.consume();
+			compiled.push(self.compiler.compile(&unit));
+		}
+		Ok(compiled)
 	}
 }
